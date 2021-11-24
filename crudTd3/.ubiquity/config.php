cc0cad710c2233aceec5eccd288608095f0fefd1
Louka Fauvel <lfauvel@sio.local>
2021-11-24 11:59:05 +0100

    td3

---
+++ crudTd3/.ubiquity/config.php
@@ -0,0 +1,2 @@
+<?php
+return array("host"=>"127.0.0.1","port"=>8090,"sessionName"=>"s619e103d1a6e6");
--- firstProject/.ubiquity/config.php
+++
@@ -1,2 +0,0 @@
-<?php
-return array("host"=>"127.0.0.1","port"=>8090,"sessionName"=>"s61920e7e9c456");
